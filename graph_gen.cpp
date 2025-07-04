2079a5747f8f56871fe80a2a13640d7ceda9864e
gush <eugene0w0gush@gmail.com>
2025-07-04 03:53:00 +0300

    Added k-clique search

--- graph_gen.cpp
+++ graph_gen.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 #include <cstdlib>
 
+#define int long long int
+
 using namespace std;
 
 void printm(vector<vector<int>> &m, ofstream &fout) {
@@ -44,6 +46,7 @@ void generate_graph(ifstream &fin, ofstream &fout) {
 
 	for(auto k : cl_sizes) {
 		vector<int> cl_idx = vector<int> (k, -1);
+		//cout << ">";
 		for (int i = 0; i < k; ++i)
 		{
 			int idx = rand() % n;
@@ -51,7 +54,9 @@ void generate_graph(ifstream &fin, ofstream &fout) {
 				idx = rand() % n;
 			}
 			cl_idx[i] = idx;
+			//cout << idx << ' ';
 		}
+		//cout << endl;
 
 		for (int i = 0; i < k; i++) {
 			for (int j = 0; j < k; j++) {
@@ -66,6 +71,98 @@ void generate_graph(ifstream &fin, ofstream &fout) {
 
 	//printm(m);
 	printm(m, fout);
+}
+
+
+int incr(int i, int k) {
+	int t = i;
+	int pos = 0;
+	while(t > 0) {
+		if(t % 2 == 1) {
+			break;
+		}
+		t /= 2;
+		pos++;
+	}
+	i += (int) pow(2, pos);
+
+	int cnt = 0;
+	t = i;
+	while(t > 0) {
+		cnt += t % 2;
+		t /= 2;
+	}
+
+	return i + (int) pow(2, k - cnt) - 1;
+}	
+
+string decToBinary(int n) {
+    string bin = "";
+    while (n > 0) {
+		int bit = n % 2;
+      	bin.push_back('0' + bit);
+        n /= 2;
+    }
+    
+	reverse(bin.begin(), bin.end());
+    return bin;
+}
+
+bool find_k_clique(ifstream &fin, int k) {
+	int n;
+	int val;
+	fin >> n;
+	vector<vector<int>> m(n);
+
+	for (int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			fin >> val;
+			m[i].push_back(val);
+		}
+	}
+
+	for(int mask = (int) pow(2, k) - 1; mask < (int) pow(2, n) - 1; mask = incr(mask, k)) {
+		if(mask % 1024 == 0) {
+			string binmask = decToBinary(mask);
+			binmask.insert(0, n-binmask.size(), '0');
+			cout << binmask << endl;
+		}
+
+		vector<int> idx;
+		int t_mask = mask;
+		int curr_size;
+		bool is_cl = true;
+
+		for(int i = 0; i < n; i++) {
+			if(t_mask % 2 == 1) idx.push_back(i);
+			t_mask = t_mask / 2;
+		}
+		curr_size = idx.size();
+
+		for (int i = 0; i < curr_size; ++i) {
+			for (int j = 0; j < curr_size; ++j)	{
+				if(idx[i] == idx[j]) {
+					continue;
+				}
+				if(m[idx[i]][idx[j]] == 0) {
+					is_cl = false;
+					break;
+				}
+			}
+			if(!is_cl) break;
+		}
+
+		if(is_cl) {
+			cout << "Found " << k << "-clique on vertices: " << endl;
+			for(auto i : idx) {
+				cout << i << " ";
+			}
+			cout << endl;
+			return true;
+		}
+	}
+	cout << k << "-clique was not found!" << endl;
+	return false;
 }
 
 void find_max_clique(ifstream &fin) {
@@ -83,7 +180,7 @@ void find_max_clique(ifstream &fin) {
 
 	int m_cl = 0;
 	int m_mask = 0;
-	for(int mask = 0; mask < (long long int) pow(2, n); mask++) {
+	for(int mask = 0; mask < (int) pow(2, n); mask++) {
 		vector<int> idx;
 		bool is_cl = true;
 		int curr_size;
@@ -116,7 +213,7 @@ void find_max_clique(ifstream &fin) {
 	}
 
 	cout << "Max cl: " << m_cl << endl;
-	cout << "Vertices: ";
+	cout << "Vertices: " << endl;
 	for(int i = 0; i < n; i++) {
 		if(m_mask % 2 == 1) cout << i << " ";
 
@@ -125,11 +222,11 @@ void find_max_clique(ifstream &fin) {
 	cout << endl;
 }
 
-int main()
+int32_t main()
 {
 	int seed = time(0);
-	seed = 1751482608;
-	//cout << seed << endl;
+	seed = 1751589752;
+	cout << seed << endl;
 
 	srand(seed);
 
@@ -137,12 +234,14 @@ int main()
 	ofstream fout("adj_matrices.txt");	
 
 	ifstream fadj("adj_matrices.txt");
+	ifstream fcli("adj_matrices.txt");
 
 	int t;
 	fin >> t;
 	for (int i = 0; i < t; ++i) {
 		generate_graph(fin, fout);
 		find_max_clique(fadj);
+		find_k_clique(fcli, 13);
 	}
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -85,6 +85,8 @@ struct Matrix {
 };
 
 bool isCopositive(Matrix &v, int depth, int &maxDepth, int &timer) {
+	if(isCopositiveCalls % 1000 == 0) cout << isCopositiveCalls << " " << depth << " " << maxDepth << " " << v.size << endl;
+
 	isCopositiveCalls++;
 
 	int n = v.size;
@@ -93,6 +95,26 @@ bool isCopositive(Matrix &v, int depth, int &maxDepth, int &timer) {
 
 	if(timer % n == 0) v.fullUpdate();
 	timer++;
+
+	{
+		bool ok = true;
+		for(int i = 0; i < n; i++) {
+			if(v[i][i] == 0) ok = false;
+		}
+		if(ok && 0) {
+			for(int i = 0; i < n; i++) {
+				if(v[i][i] != 1) {
+					ld sq = 1.0 / sqrtl(v[i][i]);
+					for(int j = 0; j < n; j++) {
+						if(i == j) continue;
+						v.set(i, j, v[i][j] * sq);
+						v.set(j, i, v[j][i] * sq);
+					}
+					v.set(i, i, 1);
+				}
+			}
+		}
+	}
 
 	// Необходимые условия
 	{
@@ -103,13 +125,53 @@ bool isCopositive(Matrix &v, int depth, int &maxDepth, int &timer) {
 		}
 	}
 
+	Matrix newV(0);
 	// Достаточное условие
 	{
-		bool ok = true;
+		int cnt = 0;
+		int mini = 0;
+		vector<int> removable;
 		for(int i = 0; i < n; i++) {
-			if(v[i][i] < -v.negSums[i]) ok = false;  
+			if(v.negSums[i] == 0) removable.push_back(i);
+			if(v[i][i] + v.negSums[i] < v[mini][mini] + v.negSums[mini]) {
+				mini = i;
+			}
+			if(v[i][i] + v.negSums[i] < 0) cnt++;
 		}
-		if(ok) return true;
+		if(v[mini][mini] + v.negSums[mini] >= 0) return true;
+		else if(cnt == 1) {
+			vector<tuple<int, int, ld>> changes;
+			ld mul = -v.negSums[mini] / v[mini][mini];
+			for(int i = 0; i < n; i++) {
+				changes.push_back(v.set(i, mini, v[i][mini] * mul));
+				changes.push_back(v.set(mini, i, v[mini][i] * mul));
+			}
+			int minj = 0;
+			for(int i = 1; i < n; i++) {
+				if(v[i][i] + v.negSums[i] < v[minj][minj] + v.negSums[minj]) {
+					minj = i;
+				}
+			}
+			reverse(changes.begin(), changes.end());
+			for(auto [i, j, val] : changes) {
+				v.set(i, j, val);
+			}
+			if(v[minj][minj] + v.negSums[minj] >= 0) return true;
+		}
+		if(removable.size()) {
+			vector<int> g(n);
+			iota(g.begin(), g.end(), 0);
+			for(auto x : removable) {
+				g.erase(find(g.begin(), g.end(), x));
+			}
+			int m = g.size();
+			newV = Matrix(g.size());
+			for(int i = 0; i < m; i++) {
+				for(int j = 0; j < m; j++) {
+					newV.set(i, j, v[g[i]][g[j]]);
+				}
+			}
+		}
 	}
 
 	int r = 0;
@@ -126,7 +188,7 @@ bool isCopositive(Matrix &v, int depth, int &maxDepth, int &timer) {
 		}
 	}
 
-	ld alpha = 0.5;
+	ld alpha = (v[c][c] - v[r][c]) / (v[r][r] - v[r][c] * 2 + v[c][c]);
 	for(int _ = 0; _ < 2; _++) {
 		vector<tuple<int, int, ld>> changes(n * 2);
 		for(int i = 0; i < n; i++) {
@@ -136,7 +198,7 @@ bool isCopositive(Matrix &v, int depth, int &maxDepth, int &timer) {
 			changes[n + i] = v.set(i, r, v[i][r] * alpha + v[i][c] * (1 - alpha));
 		}
 		v.changesUpdate();
-		if(!isCopositive(v, depth + 1, maxDepth, timer)) return false;
+		if(!isCopositive((newV.size ? newV : v), depth + 1, maxDepth, timer)) return false;
 
 		reverse(changes.begin(), changes.end());
 		for(auto [i, j, val] : changes) {
@@ -176,23 +238,42 @@ bool solveForCopositive(ifstream &fin) {
 	return (copositive == expected);
 }
 
+bool isCopositiveForCliqueSize(vector<vector<int>> &g, int cliqueSize) {
+	int n = g.size();
+	Matrix v(n);
+	ld currAlpha = (ld) (cliqueSize - 1) / cliqueSize;
+	ld nextAlpha = (ld) cliqueSize / (cliqueSize + 1);
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			ld alpha = (currAlpha + nextAlpha) / 2;
+			v.set(i, j, alpha - g[i][j]);
+		}
+	}
+	int maxDepth = 0;
+	int timer = 0;
+	int temp = isCopositiveCalls;
+	bool copositive = isCopositive(v, 0, maxDepth, timer);
+	return copositive;
+}
+
 int findCliqueNumber(vector<vector<int>> &g) {
 	int n = g.size();
+
+	for(int i = n; i >= 1; i--) {
+		int temp = isCopositiveCalls;
+		bool copositive = isCopositiveForCliqueSize(g, i);
+		if(copositive) {
+			cout << "YES ";
+		} else cout << "NO ";
+		cout << isCopositiveCalls - temp << endl;
+	}
 
 	int l = 1, r = n;
 	while(l <= r) {
 		int mid = (l + r) / 2;
-		Matrix v(n);
-		ld currAlpha = (ld) (mid - 1) / mid;
-		ld nextAlpha = (ld) mid / (mid + 1);
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				v.set(i, j, (currAlpha + nextAlpha) / 2 - g[i][j]);
-			}
-		}
-		int maxDepth = 0;
-		int timer = 0;
-		bool copositive = isCopositive(v, 0, maxDepth, timer);
+		int temp = isCopositiveCalls;
+		bool copositive = isCopositiveForCliqueSize(g, mid);
+		cout << mid << endl;
 		if(copositive) r = mid - 1;
 		else l = mid + 1;
 	}
